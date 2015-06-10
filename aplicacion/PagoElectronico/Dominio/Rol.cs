e0706d19f3b53524d4e6d19a0f0386020534b540
nachocir93 <uzumymw19>
2015-06-10 16:57:35 -0300

    ABM Rol

---
+++ aplicacion/PagoElectronico/Dominio/Rol.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace PagoElectronico.Dominio
+{
+    class Rol
+    {
+        private int idRol;
+        public String nombreRol { get; set; }
+        public bool estado { get; set; }
+
+        public Rol(SqlDataReader reader) 
+        {           
+            idRol = Convert.ToInt32(reader["idRol"].ToString());
+            nombreRol = reader["nombreRol"].ToString();
+            estado = Boolean.Parse(reader["estado"].ToString());            
+        }
+
+        public Rol(String id, String nombre, String est)
+        {
+            idRol = Convert.ToInt32(id);
+            nombreRol = nombre;
+            estado = Boolean.Parse(est);
+        }
+
+        public int getId() 
+        {
+            return idRol;
+        }
+    }
+}
+
