1e96ebb8d7139867676d1f312c41b8933061d3be
Isabel <isaleb.17@gmail.com>
2015-06-12 01:46:40 -0300

    Services de Usuario y Rol

---
+++ aplicacion/PagoElectronico/Services/RolServices.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PagoElectronico.DAO;
+using System.Data.SqlClient;
+using System.Data;
+using PagoElectronico.Dominio;
+using System.Windows.Forms;
+
+namespace PagoElectronico.Services
+{
+    static class RolServices
+    {
+        public static DataTable getRoles()
+        {
+            SqlConnection conn = ConexionDB.conectarDB();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommand command = new SqlCommand();
+            try{
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "SUDO.SP_LISTAR_ROLES";
+                command.Connection = conn;
+                adapter.SelectCommand = command;
+
+                DataTable tb = new DataTable();
+                adapter.Fill(tb);
+                return (tb);
+            }
+            catch (Exception ex){
+                throw new Exception(ex.Message);
+
+            }
+            finally{
+                conn.Dispose();
+                command.Dispose();
+            }
+        }
+    }
+}
---
+++ aplicacion/PagoElectronico/Services/UsuarioServices.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PagoElectronico.DAO;
+using System.Data.SqlClient;
+using System.Data;
+using PagoElectronico.Dominio;
+using System.Windows.Forms;
+
+namespace PagoElectronico.Services
+{
+    static class UsuarioServices
+    {
+        public static int SaveUser(Usuario u) {
+            SqlConnection conn = ConexionDB.conectarDB();
+            SqlCommand command = getComandoSave(u, conn);
+            try{
+                command.ExecuteScalar();
+
+                int iRdo = int.Parse(command.Parameters["@VALOR"].Value.ToString());
+
+                if (iRdo == 0){
+                    MessageBox.Show("El Usuario se ha creado correctamente");
+                }
+                if (iRdo == 1)
+                {
+                    MessageBox.Show("Existe un Usuario con ese Username, por favor ingrese otro");
+                    return 1;
+                }
+            }
+            catch (Exception ex){
+                throw new Exception(ex.Message);
+
+            }
+            finally{
+                conn.Dispose();
+                command.Dispose();
+            }
+            return 0;
+        }
+
+        private static SqlCommand getComandoSave(Usuario u, SqlConnection conn)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommand comando = new SqlCommand();
+
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = "SUDO.SP_ALTA_USUARIO";
+            comando.Connection = conn;
+            adapter.SelectCommand = comando;
+
+            SqlParameter user = new SqlParameter("@USER", SqlDbType.VarChar, 255);
+            user.Value = u.userName;
+            comando.Parameters.Add(user);
+
+            SqlParameter password = new SqlParameter("@PASSWORD", SqlDbType.VarChar, 255);
+            password.Value = u.userPassword;
+            comando.Parameters.Add(password);
+
+            SqlParameter fechaCreacion = new SqlParameter("@FECHACREA", SqlDbType.DateTime);
+            fechaCreacion.Value = u.fechaCreacion;
+            comando.Parameters.Add(fechaCreacion);
+
+            SqlParameter fechaModificacion = new SqlParameter("@FECHAMODIF", SqlDbType.DateTime);
+            fechaModificacion.Value = u.fechaUltimaModificacion;
+            comando.Parameters.Add(fechaModificacion);
+            return comando;
+
+        }
+
+
+    }
+}
