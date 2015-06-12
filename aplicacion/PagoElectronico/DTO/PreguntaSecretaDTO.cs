6f575d6f38f7886db89fabb033a62f57971ecc62
Isabel <isaleb.17@gmail.com>
2015-06-12 01:45:39 -0300

    DTO para pregunta secreta ABM Usuario

---
+++ aplicacion/PagoElectronico/DTO/PreguntaSecretaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PagoElectronico.DTO
+{
+    class PreguntaSecretaDTO
+    {
+        public int value { get; set; }
+        public String valor { get; set; }
+
+        public static PreguntaSecretaDTO build(int value, String valor) {
+            PreguntaSecretaDTO dto = new PreguntaSecretaDTO();
+            dto.value = value;
+            dto.valor = valor;
+            return dto;
+        }
+    }
+}
