ef2c2b1213a5878ecfcff81d138c6b79b974a916
Isabel <isaleb.17@gmail.com>
2015-06-27 20:35:45 -0300

    Services Usuario

--- aplicacion/PagoElectronico/Services/UsuarioServices.cs
+++ aplicacion/PagoElectronico/Services/UsuarioServices.cs
@@ -7,20 +7,25 @@ using System.Data.SqlClient;
 using System.Data;
 using PagoElectronico.Dominio;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace PagoElectronico.Services
 {
     static class UsuarioServices
     {
-        public static int SaveUser(Usuario u) {
+        public static int SaveUser(Usuario u, int rol)
+        {
             SqlConnection conn = ConexionDB.conectarDB();
-            SqlCommand command = GetComandoSave(u, conn);
-            try{
+            SqlCommand command = GetComandoSave(u, conn, rol);
+
+            try
+            {
                 command.ExecuteScalar();
 
                 int iRdo = int.Parse(command.Parameters["@VALOR"].Value.ToString());
 
-                if (iRdo == 0){
+                if (iRdo == 0)
+                {
                     MessageBox.Show("El Usuario se ha creado correctamente");
                 }
                 if (iRdo == 1)
@@ -29,18 +34,20 @@ namespace PagoElectronico.Services
                     return 1;
                 }
             }
-            catch (Exception ex){
+            catch (Exception ex)
+            {
                 throw new Exception(ex.Message);
 
             }
-            finally{
+            finally
+            {
                 conn.Dispose();
                 command.Dispose();
             }
             return 0;
         }
 
-        private static SqlCommand GetComandoSave(Usuario u, SqlConnection conn)
+        private static SqlCommand GetComandoSave(Usuario u, SqlConnection conn, int rol)
         {
             SqlDataAdapter adapter = new SqlDataAdapter();
             SqlCommand comando = new SqlCommand();
@@ -50,9 +57,13 @@ namespace PagoElectronico.Services
             comando.Connection = conn;
             adapter.SelectCommand = comando;
 
-            SqlParameter user = new SqlParameter("@USER", SqlDbType.VarChar, 255);
+            SqlParameter user = new SqlParameter("@USUARIO", SqlDbType.VarChar, 255);
             user.Value = u.userName;
             comando.Parameters.Add(user);
+
+            SqlParameter rolDB = new SqlParameter("@ROL", SqlDbType.Int);
+            rolDB.Value = rol;
+            comando.Parameters.Add(rolDB);
 
             SqlParameter password = new SqlParameter("@PASSWORD", SqlDbType.VarChar, 255);
             password.Value = u.userPassword;
@@ -90,38 +101,71 @@ namespace PagoElectronico.Services
 
         }
 
-        public static DataTable GetUserByFiltros(String user, int rolAsignado, DateTime fechaAlta, DateTime fechaModif) {
+        public static DataTable GetUserByFiltros(String user, int rolAsignado, DateTime? fechaAlta, DateTime? fechaModif)
+        {
             SqlConnection conn = ConexionDB.conectarDB();
             SqlDataAdapter adapter = new SqlDataAdapter();
             SqlCommand comando = new SqlCommand();
 
             comando.CommandType = CommandType.StoredProcedure;
-            comando.CommandText = "GR46.SUDO_LISTADO_USERS";
+            comando.CommandText = "SUDO.SP_LISTADO_USUARIOS";
             comando.Connection = conn;
             adapter.SelectCommand = comando;
 
-            SqlParameter userDB = new SqlParameter("@USER", SqlDbType.VarChar, 255);
+            SqlParameter userDB = new SqlParameter("@USUARIO", SqlDbType.VarChar, 255);
             if (user == "")
             {
                 userDB.Value = DBNull.Value;
-            }else
+            }
+            else
             {
                 userDB.Value = user;
             }
             comando.Parameters.Add(userDB);
 
             SqlParameter rolAsignadoDB = new SqlParameter("@ROL", SqlDbType.Int);
-            rolAsignadoDB.Value = rolAsignado;
+            if (rolAsignado == 0)
+            {
+                rolAsignadoDB.Value = DBNull.Value;
+            }
+            else
+            {
+                rolAsignadoDB.Value = rolAsignado;
+            }
             comando.Parameters.Add(rolAsignadoDB);
 
             SqlParameter fechaAltaDB = new SqlParameter("@FECHAALTA", SqlDbType.DateTime);
-            fechaAltaDB.Value = fechaAlta;
+            if (fechaAlta == null)
+            {
+                fechaAltaDB.Value = DBNull.Value;
+            }
+            else
+            {
+                //DateTime fa = DateTime.ParseExact(fechaAlta.ToString(), "dd/MM/yyyy hh:mm:ss tt", CultureInfo.InvariantCulture, DateTimeStyles.None);
+                String fa = String.Format("{0:u}", fechaAlta.ToString());
+                fechaAltaDB.Value = fechaAlta;
+            }
             comando.Parameters.Add(fechaAltaDB);
 
             SqlParameter fechaModifDB = new SqlParameter("@FECHAMODIF", SqlDbType.DateTime);
-            fechaModifDB.Value = fechaModif;
+            if (fechaModif == null)
+            {
+                fechaModifDB.Value = DBNull.Value;
+            }
+            else
+            {
+                fechaModifDB.Value = fechaModif;
+            }
             comando.Parameters.Add(fechaModifDB);
 
+            string debugSQL = comando.CommandText;
+
+            foreach (SqlParameter param in comando.Parameters)
+            {
+                param.SqlValue.ToString();
+                param.ToString();
+
+            }
             try
             {
                 DataTable tb = new DataTable();
